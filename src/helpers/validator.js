da9239f9876ea122b983304c18767b15a96348d0
jaspreetkaur03 <jaspreetkaur03@nagarro.com>
2023-05-10 20:30:10 +0530

    Fixed Test Cases

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,58 +1,87 @@
-class validator{
-    static validateNewUserData(new_userData,usersData){
-        if(new_userData.hasOwnProperty("id")&&
-        new_userData.hasOwnProperty("email")&&
-        new_userData.hasOwnProperty("password")&&
-        new_userData.hasOwnProperty("preferences")&&this.validateUniqueEmail(new_userData,usersData) && this.validateEmailPattern(new_userData)
-        && this.validatePasswordPattern(new_userData)){
-            console.log(new_userData.email)
-            return{
-                "status":true,
-                "message":"User Registered successfully"
-            };
-        }
-            if(!this.validateUniqueEmail(new_userData,usersData)){
-                return{
-                    "status":false,
-                    "message":"email has to be unique"
-                };
-            }
-            if(!this.validateEmailPattern(new_userData) && new_userData.email!=undefined){
-                return{
-                    "status":false,
-                    "message":"Invalid Email Pattern"
-                };
-            }
-
-            if(!this.validatePasswordPattern(new_userData) && new_userData.password!=undefined){
-                return {
-                    "status":false,
-                    "message":"Password should be atleast 8 characters long, has at least 1 special character, 1 digit, 1 lowercase and 1 uppercase"
-                }
-            }
-
-            return{
-                "status":false,
-                "message":"user details are incomplete,please provide all properties"
-            };
-
-        }
-
-        static validateUniqueEmail(new_userData,usersData){
-            let existing_userFound=usersData.users.some(val=>val.email==new_userData.email);
-            if (existing_userFound) return false;
-            else return true;
-        }
-
-        static validateEmailPattern(new_userData){
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            return emailRegex.test(new_userData.email) ;
-        }
-
-        static validatePasswordPattern(new_userData){
-            const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/;
-            return passwordRegex.test(new_userData.password);
-        }
+class validator {
+    static validateNewUserData(new_userData, usersData) {
+      new_userData = this.sanitizeInputData(new_userData);
+      if (
+        new_userData.hasOwnProperty("id") &&
+        new_userData.hasOwnProperty("email") &&
+        new_userData.hasOwnProperty("password") &&
+        new_userData.hasOwnProperty("preferences") &&
+        this.validateUniqueEmail(new_userData, usersData) &&
+        this.validateEmailPattern(new_userData) &&
+        this.validatePasswordPattern(new_userData)
+      ) {
+        console.log(new_userData.email);
+        return {
+          status: true,
+          message: "User Registered successfully",
+        };
+      }
+      if (!this.validateUniqueEmail(new_userData, usersData)) {
+        return {
+          status: false,
+          message: "email has to be unique",
+        };
+      }
+      if (!this.validateEmailPattern(new_userData)) {
+        return {
+          status: false,
+          message: "Invalid Email Pattern",
+        };
+      }
+  
+      if (!this.validatePasswordPattern(new_userData)) {
+        return {
+          status: false,
+          message:
+            "Password should be atleast 8 characters long, has at least 1 special character, 1 digit, 1 lowercase and 1 uppercase",
+        };
+      }
+  
+      return {
+        status: false,
+        message: "user details are incomplete,please provide all properties",
+      };
     }
-
-module.exports=validator
+  
+    static validateUniqueEmail(new_userData, usersData) {
+      let existing_userFound = usersData.users.some(
+        (val) => val.email == new_userData.email
+      );
+      if (existing_userFound) return false;
+      else return true;
+    }
+  
+    static validateEmailPattern(new_userData) {
+      if (new_userData.email != undefined) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(new_userData.email);
+      } else {
+        return false;
+      }
+    }
+  
+    static validatePasswordPattern(new_userData) {
+      if (new_userData.password != undefined) {
+        const passwordRegex =
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/;
+        return passwordRegex.test(new_userData.password);
+      } else {
+        return false;
+      }
+    }
+  
+    static sanitizeInputData(inputData) {
+      const sanitizedData = {};
+      for (const key in inputData) {
+        if (typeof inputData[key] == "string") {
+          sanitizedData[key] = inputData[key].replace(/<[^>]*>?/gm, "");
+        } else {
+          sanitizedData[key] = inputData[key];
+        }
+      }
+      return sanitizedData;
+    }
+  }
+  
+  module.exports = validator;
+  
--- test/helpers/validator.test.js
+++ test/helpers/validator.test.js
@@ -1,201 +1,420 @@
 var User = require("../../src/models/users.json");
-let chai = require('chai');
-let chaiHttp = require('chai-http');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
 chai.use(chaiHttp);
-const server = require('../../app');
-const expect = require('chai').expect;
-const fs = require('fs');
-const path=require('path')
+const server = require("../../app");
+const expect = require("chai").expect;
+const fs = require("fs");
+const path = require("path");
+const jwt = require("jsonwebtoken");
 
-describe('News Aggregator Tests ', ()=>{
-
-    let token = null;
-    it("Succesfull Signup", (done)=>{
-        let userDetails={
-            "email": "td@gmail.com",
-            "password": "$2b$08$MqvQCbor9e3LKdCC",
-            "preferences": "sports"
-        };        
-        chai.request(server).post('/register').send(userDetails).end((err,res) => {
-            expect(res.status).equal(200);
-            expect(res.body.message).equal('User Registered successfully');  
-            done();
-                let current_users = JSON.parse(JSON.stringify(User))
-                current_users.users = current_users.users.filter(user => user.email !== userDetails.email);
-                const write_path=path.join(__dirname,'../..','/src/models/users.json')
-                fs.writeFileSync(write_path, JSON.stringify(current_users));
-        })
-    })
-
-    it("UnSuccesfull Signup due to Invalid Email", (done)=>{
-        let userDetails={
-            "email": "tdgmailcom",
-            "password": "$2b$08$MqvQCbor9e3LKdCC",
-            "preferences": "sports"
-        };        
-        chai.request(server).post('/register').send(userDetails).end((err,res) => {
-            expect(res.status).equal(400);
-            expect(res.body.message).equal('Invalid Email Pattern');  
-            done();
-                
-        })
-    })
-
-    it("UnSuccesfull Signup due to weak Password", (done)=>{
-        let userDetails={
-            "email": "td@gmail.com",
-            "password": "1234",
-            "preferences": "sports"
-        };        
-        chai.request(server).post('/register').send(userDetails).end((err,res) => {
-            expect(res.status).equal(400)
-            expect(res.body.message).equal('Password should be atleast 8 characters long, has at least 1 special character, 1 digit, 1 lowercase and 1 uppercase');  
-            done();
-                
-        })
-    })
-
-
-
-
-
-    it("Successfull Signin", (done)=>{
-        let userCredentials={
-            "email":"dhireshg@gmail.com",
-            "password":"123456"
-        }
-        chai.request(server).post('/signin').send(userCredentials).end((err,res) => {
-            expect(res.status).equal(200);
-            expect(res.body.message).equal('Login Successfull');
-            token = res.body.accessToken;
-            done();
-        })
-    }) 
-    
-    it("UnSuccessfull Signin due to missing details", (done)=>{
-        let userCredentials={
-            "email":"dhireshg@gmail.com",
-        }
-        chai.request(server).post('/signin').send(userCredentials).end((err,res) => {
-            expect(res.status).equal(401);
-            expect(res.body.message).equal('Incomplete details');
-          //  token = res.body.accessToken;
-            done();
-        })
-    }) 
-
-    it("UnSuccessfull Signin due to Wrong Password or email", (done)=>{
-        let userCredentials={
-            "email":"dhireshg@gmail.com",
-            "password":"jefihfiehfd"
-        }
-        chai.request(server).post('/signin').send(userCredentials).end((err,res) => {
-            expect(res.status).equal(401);
-            expect(res.body.message).equal('Invalid username or password');
-          //  token = res.body.accessToken;
-            done();
-        })
-    }) 
-
-
-    
-    
-
-
-    it('Get News According to preferences',(done)=>{
-       // console.log(`JWT ${token}`);
-        let userCredentials = {
-            "email":"dhireshg@gmail.com"
-        }
-        chai.request(server).get('/news').send(userCredentials)
-        .set('Authorization',`JWT ${token}`)
-        .end((err,res)=>{
-            if(err){
-                console.log(err);
-                done(err);
-            }
-            else{
-            expect(res.status).to.have.equal(200);
-            done();}
-        })
-    })
-
-
-    it('Should return 403 if JWT token is invalid', (done) => {
-        let userCredentials = {
-            "email":"dhireshg@gmail.com"
-        }
-        chai.request(server)
-          .get('/news').send(userCredentials)
-          .set('Authorization', 'JWT invalid_token')
-          .end((err, res) => {
-            expect(res).to.have.status(403);
-            expect(res.body).to.have.property('message', 'Invalid JWT token');
-            done();
-          });
+describe("Sign Up and SignIn Tests", () => {
+  it("Succesfull Signup", (done) => {
+    let userDetails = {
+      email: "td@gmail.com",
+      password: "$2b$08$MqvQCbor9e3LKdCC",
+      preferences: "sports",
+    };
+    chai
+      .request(server)
+      .post("/register")
+      .send(userDetails)
+      .end((err, res) => {
+        expect(res.status).equal(200);
+        expect(res.body.message).equal("User Registered successfully");
+        done();
+        let current_users = JSON.parse(JSON.stringify(User));
+        current_users.users = current_users.users.filter(
+          (user) => user.email !== userDetails.email
+        );
+        const write_path = path.join(
+          __dirname,
+          "../..",
+          "/src/models/users.json"
+        );
+        fs.writeFileSync(write_path, JSON.stringify(current_users));
       });
+  });
 
-      it('Should return 403 if JWT token is missing', (done) => {
-        let userCredentials = {
-            "email":"dhireshg@gmail.com"
-        }
-        chai.request(server)
-          .get('/news').send(userCredentials)
-          .set('Authorization', '')
-          .end((err, res) => {
-            expect(res).to.have.status(403);
-            expect(res.body).to.have.property('message', 'Authorization header not found');
-            done();
-          });
+  it("UnSuccesfull Signup due to Invalid Email", (done) => {
+    let userDetails = {
+      email: "tdgmailcom",
+      password: "$2b$08$MqvQCbor9e3LKdCC",
+      preferences: "sports",
+    };
+    chai
+      .request(server)
+      .post("/register")
+      .send(userDetails)
+      .end((err, res) => {
+        expect(res.status).equal(400);
+        expect(res.body.message).equal("Invalid Email Pattern");
+        done();
       });
+  });
 
+  it("UnSuccesfull Signup due to weak Password", (done) => {
+    let userDetails = {
+      email: "td@gmail.com",
+      password: "1234",
+      preferences: "sports",
+    };
+    chai
+      .request(server)
+      .post("/register")
+      .send(userDetails)
+      .end((err, res) => {
+        expect(res.status).equal(400);
+        expect(res.body.message).equal(
+          "Password should be atleast 8 characters long, has at least 1 special character, 1 digit, 1 lowercase and 1 uppercase"
+        );
+        done();
+      });
+  });
 
-    it('Get preferences of User',(done)=>{
-       // console.log(`JWT ${token}`);
-        let userCredentials = {
-            "email":"dhireshg@gmail.com"
+  it("Successfull Signin", (done) => {
+    let userCredentials = {
+      email: "dhireshg@gmail.com",
+      password: "123456",
+    };
+    chai
+      .request(server)
+      .post("/signin")
+      .send(userCredentials)
+      .end((err, res) => {
+        expect(res.status).equal(200);
+        expect(res.body.message).equal("Login Successfull");
+        // token = res.body.accessToken;
+        done();
+      });
+  });
+
+  it("UnSuccessfull Signin due to missing details", (done) => {
+    let userCredentials = {
+      email: "dhireshg@gmail.com",
+    };
+    chai
+      .request(server)
+      .post("/signin")
+      .send(userCredentials)
+      .end((err, res) => {
+        expect(res.status).equal(401);
+        expect(res.body.message).equal("Incomplete details");
+        //  token = res.body.accessToken;
+        done();
+      });
+  });
+
+  it("UnSuccessfull Signin due to Wrong Password or email", (done) => {
+    let userCredentials = {
+      email: "dhireshg@gmail.com",
+      password: "jefihfiehfd",
+    };
+    chai
+      .request(server)
+      .post("/signin")
+      .send(userCredentials)
+      .end((err, res) => {
+        expect(res.status).equal(401);
+        expect(res.body.message).equal("Invalid username or password");
+        //  token = res.body.accessToken;
+        done();
+      });
+  });
+});
+
+describe("News Aggregator Tests with SuccessFul sign in  ", () => {
+  //  let token = null;
+  let userCredentials = {
+    email: "dhireshg@gmail.com",
+  };
+  let userId = "2c5a57c4-cf85-48f6-a8dd-5a73b518eb65";
+  token = jwt.sign(
+    {
+      id: userId,
+    },
+    process.env.API_SECRET,
+    {
+      expiresIn: 86400,
+    }
+  );
+
+  it("Get News According to preferences", (done) => {
+    chai
+      .request(server)
+      .get("/news")
+      .send(userCredentials)
+      .set("Authorization", `JWT ${token}`)
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+          done(err);
+        } else {
+          expect(res.status).to.have.equal(200);
+          done();
         }
-        chai.request(server).get('/news/preferences').send(userCredentials)
-        .set('Authorization',`JWT ${token}`)
-        .end((err,res)=>{
-            if(err){
-                console.log(err);
-                done(err);
-            }
-            else{
-            expect(res.status).to.have.equal(200);
-            done();}
-        })
-    })
+      });
+  });
 
-    it('Should update tasks',(done)=>{
-      //  console.log(`JWT ${token}`);
-        let userCredentials = {
-            "email":"dhireshg@gmail.com",
-            "preferences":"technology"
+  it("Get preferences of User", (done) => {
+    // console.log(`JWT ${token}`);
+
+    chai
+      .request(server)
+      .get("/news/preferences")
+      .send(userCredentials)
+      .set("Authorization", `JWT ${token}`)
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+          done(err);
+        } else {
+          expect(res.status).to.have.equal(200);
+          done();
         }
-        chai.request(server).put('/news/preferences').send(userCredentials)
-        .set('Authorization',`JWT ${token}`)
-        .end((err,res)=>{
-            if(err){
-                console.log(err);
-                done(err);
-            }
-            else{
-            expect(res.status).to.have.equal(200)
-            expect(res.body.success).to.equal(true)
-            expect(res.body.message).to.equal('Task updated')
-            done();}
-        })
-    })
+      });
+  });
 
+  it("Should update tasks", (done) => {
+    //  console.log(`JWT ${token}`);
+    let userPrefCredentials = {
+      email: "dhireshg@gmail.com",
+      preferences: "technology",
+    };
+    chai
+      .request(server)
+      .put("/news/preferences")
+      .send(userPrefCredentials)
+      .set("Authorization", `JWT ${token}`)
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+          done(err);
+        } else {
+          expect(res.status).to.have.equal(200);
+          expect(res.body.success).to.equal(true);
+          expect(res.body.message).to.equal("Task updated");
+          done();
+        }
+      });
+  });
+});
 
+describe("News Aggregator Tests with UnSuccessFul sign in  ", () => {
+  it("Should return 403 if JWT token is invalid", (done) => {
+    let userCredentials = {
+      email: "dhireshg@gmail.com",
+    };
+    chai
+      .request(server)
+      .get("/news")
+      .send(userCredentials)
+      .set("Authorization", "JWT invalid_token")
+      .end((err, res) => {
+        expect(res).to.have.status(403);
+        expect(res.body).to.have.property("message", "Invalid JWT token");
+        done();
+      });
+  });
 
+  it("Should return 403 if JWT token is missing", (done) => {
+    let userCredentials = {
+      email: "dhireshg@gmail.com",
+    };
+    chai
+      .request(server)
+      .get("/news")
+      .send(userCredentials)
+      .set("Authorization", "")
+      .end((err, res) => {
+        expect(res).to.have.status(403);
+        expect(res.body).to.have.property(
+          "message",
+          "Authorization header not found"
+        );
+        done();
+      });
+  });
+});
 
+//   var User = require("../../src/models/users.json");
+// let chai = require('chai');
+// let chaiHttp = require('chai-http');
+// chai.use(chaiHttp);
+// const server = require('../../app');
+// const expect = require('chai').expect;
+// const fs = require('fs');
+// const path=require('path')
 
-   
+// describe('News Aggregator Tests ', ()=>{
 
+//     let token = null;
+//     it("Succesfull Signup", (done)=>{
+//         let userDetails={
+//             "email": "td@gmail.com",
+//             "password": "$2b$08$MqvQCbor9e3LKdCC",
+//             "preferences": "sports"
+//         };
+//         chai.request(server).post('/register').send(userDetails).end((err,res) => {
+//             expect(res.status).equal(200);
+//             expect(res.body.message).equal('User Registered successfully');
+//             done();
+//                 let current_users = JSON.parse(JSON.stringify(User))
+//                 current_users.users = current_users.users.filter(user => user.email !== userDetails.email);
+//                 const write_path=path.join(__dirname,'../..','/src/models/users.json')
+//                 fs.writeFileSync(write_path, JSON.stringify(current_users));
+//         })
+//     })
 
-})
+//     it("UnSuccesfull Signup due to Invalid Email", (done)=>{
+//         let userDetails={
+//             "email": "tdgmailcom",
+//             "password": "$2b$08$MqvQCbor9e3LKdCC",
+//             "preferences": "sports"
+//         };
+//         chai.request(server).post('/register').send(userDetails).end((err,res) => {
+//             expect(res.status).equal(400);
+//             expect(res.body.message).equal('Invalid Email Pattern');
+//             done();
 
+//         })
+//     })
 
+//     it("UnSuccesfull Signup due to weak Password", (done)=>{
+//         let userDetails={
+//             "email": "td@gmail.com",
+//             "password": "1234",
+//             "preferences": "sports"
+//         };
+//         chai.request(server).post('/register').send(userDetails).end((err,res) => {
+//             expect(res.status).equal(400)
+//             expect(res.body.message).equal('Password should be atleast 8 characters long, has at least 1 special character, 1 digit, 1 lowercase and 1 uppercase');
+//             done();
+
+//         })
+//     })
+
+//     it("Successfull Signin", (done)=>{
+//         let userCredentials={
+//             "email":"dhireshg@gmail.com",
+//             "password":"123456"
+//         }
+//         chai.request(server).post('/signin').send(userCredentials).end((err,res) => {
+//             expect(res.status).equal(200);
+//             expect(res.body.message).equal('Login Successfull');
+//             token = res.body.accessToken;
+//             done();
+//         })
+//     })
+
+//     it("UnSuccessfull Signin due to missing details", (done)=>{
+//         let userCredentials={
+//             "email":"dhireshg@gmail.com",
+//         }
+//         chai.request(server).post('/signin').send(userCredentials).end((err,res) => {
+//             expect(res.status).equal(401);
+//             expect(res.body.message).equal('Incomplete details');
+//           //  token = res.body.accessToken;
+//             done();
+//         })
+//     })
+
+//     it("UnSuccessfull Signin due to Wrong Password or email", (done)=>{
+//         let userCredentials={
+//             "email":"dhireshg@gmail.com",
+//             "password":"jefihfiehfd"
+//         }
+//         chai.request(server).post('/signin').send(userCredentials).end((err,res) => {
+//             expect(res.status).equal(401);
+//             expect(res.body.message).equal('Invalid username or password');
+//           //  token = res.body.accessToken;
+//             done();
+//         })
+//     })
+
+//     it('Get News According to preferences',(done)=>{
+//        // console.log(`JWT ${token}`);
+//         let userCredentials = {
+//             "email":"dhireshg@gmail.com"
+//         }
+//         chai.request(server).get('/news').send(userCredentials)
+//         .set('Authorization',`JWT ${token}`)
+//         .end((err,res)=>{
+//             if(err){
+//                 console.log(err);
+//                 done(err);
+//             }
+//             else{
+//             expect(res.status).to.have.equal(200);
+//             done();}
+//         })
+//     })
+
+//     it('Should return 403 if JWT token is invalid', (done) => {
+//         let userCredentials = {
+//             "email":"dhireshg@gmail.com"
+//         }
+//         chai.request(server)
+//           .get('/news').send(userCredentials)
+//           .set('Authorization', 'JWT invalid_token')
+//           .end((err, res) => {
+//             expect(res).to.have.status(403);
+//             expect(res.body).to.have.property('message', 'Invalid JWT token');
+//             done();
+//           });
+//       });
+
+//       it('Should return 403 if JWT token is missing', (done) => {
+//         let userCredentials = {
+//             "email":"dhireshg@gmail.com"
+//         }
+//         chai.request(server)
+//           .get('/news').send(userCredentials)
+//           .set('Authorization', '')
+//           .end((err, res) => {
+//             expect(res).to.have.status(403);
+//             expect(res.body).to.have.property('message', 'Authorization header not found');
+//             done();
+//           });
+//       });
+
+//     it('Get preferences of User',(done)=>{
+//        // console.log(`JWT ${token}`);
+//         let userCredentials = {
+//             "email":"dhireshg@gmail.com"
+//         }
+//         chai.request(server).get('/news/preferences').send(userCredentials)
+//         .set('Authorization',`JWT ${token}`)
+//         .end((err,res)=>{
+//             if(err){
+//                 console.log(err);
+//                 done(err);
+//             }
+//             else{
+//             expect(res.status).to.have.equal(200);
+//             done();}
+//         })
+//     })
+
+//     it('Should update tasks',(done)=>{
+//       //  console.log(`JWT ${token}`);
+//         let userCredentials = {
+//             "email":"dhireshg@gmail.com",
+//             "preferences":"technology"
+//         }
+//         chai.request(server).put('/news/preferences').send(userCredentials)
+//         .set('Authorization',`JWT ${token}`)
+//         .end((err,res)=>{
+//             if(err){
+//                 console.log(err);
+//                 done(err);
+//             }
+//             else{
+//             expect(res.status).to.have.equal(200)
+//             expect(res.body.success).to.equal(true)
+//             expect(res.body.message).to.equal('Task updated')
+//             done();}
+//         })
+//     })
+
+// })
